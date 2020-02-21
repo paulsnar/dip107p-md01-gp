4f0c107e5b9ffcf16a8a03b4e11f3b9ac6ed8375
paulsnar <paulsnar@paulsnar.lv>
2020-02-21 12:18:09 +0200

    Add Quicksort2
    The main difference is that it uses an ArrayWindow and therefore avoids
    making one copy at the expense of many objects. This is marginally
    faster than the other version if the JIT isn't off.

---
+++ Quicksort2.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+
+/**
+ * @author Pauls Nartišs
+ */
+class Quicksort2 implements Sorter {
+  public int[] sort(int[] array) {
+    return sort(new ArrayWindow(array)).getArray();
+  }
+
+  private Random random = new Random();
+  public ArrayWindow sort(ArrayWindow window) {
+    int size = window.size();
+    if (size < 2) {
+      return window;
+    }
+
+    int pivotIndex = random.nextInt(size),
+        pivot = window.get(pivotIndex);
+    int left = 0, right = window.size() - 1;
+    while (left <= right) {
+      while (window.get(left) < pivot) left += 1;
+      while (window.get(right) > pivot) right -= 1;
+      if (left > right) {
+        break;
+      }
+      window.swap(left, right);
+      left += 1;
+      right -= 1;
+    }
+
+    if (right > 0) {
+      sort(window.slice(0, right));
+    }
+    if (left < size - 1) {
+      sort(window.slice(left, 0));
+    }
+    return window;
+  }
+}
