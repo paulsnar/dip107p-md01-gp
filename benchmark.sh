1b6b441812ae5607e3fa75781b87fbb4f2b048f9
paulsnar <paulsnar@paulsnar.lv>
2020-02-21 16:01:17 +0200

    Reenable GC for benchmark runs
    JVM runs out of memory and I can't be bothered to tweak the heap sizes,
    I'm not a GC operator :)

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 # -Xint disables the JIT compiler which makes for more predictable performance
-# -XX:+UseEpsilonGC functionally disables garbage collection
-exec java -Xint -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC Runner $@
+exec java -Xint Runner $@
